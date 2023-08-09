7fae63510e6a98b733fb1c9b7de88f6e12b8c7a5
Mateusz Mazgaj <m.mazgaj@outlook.com>
2023-08-09 23:18:32 +0200

    adjust lsp-zero plugin

--- plugin/lsp-zero.lua
+++ plugin/lsp-zero.lua
@@ -11,10 +11,15 @@ return {
 		{ 'hrsh7th/nvim-cmp' }, -- Required
 		{ 'hrsh7th/cmp-nvim-lsp' }, -- Required
 		{ 'L3MON4D3/LuaSnip' }, -- Required
+		{ 'hrsh7th/cmp-buffer'},
+		{ 'hrsh7th/cmp-path'},
+
 	},
 	config = function()
 		local lsp = require('lsp-zero').preset({})
-
+		lsp.manage_nvim_cmp = {
+			set_basic_mappings = true
+		}
 		lsp.on_attach(function(client, bufnr)
 			-- see :help lsp-zero-keybindings
 			-- to learn the available actions
@@ -25,5 +30,13 @@ return {
 		require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
 		lsp.setup()
+
+		local cmp = require('cmp')
+		cmp.sources = {
+			{ name = 'nvim_lsp' },
+			{ name = 'buffer' },
+			{name = 'path'},
+			{name = 'luasnip'},
+		}
 	end
 }
